8d29c2f211ecdfd667a9ebdfdb53406d156bf8ae
MattHamada <ermacaz@gmail.com>
2013-08-06 15:33:24 -0700

    started rspec tests

---
+++ spec/controllers/movies_controller_spec.rb
@@ -0,0 +1,10 @@
+require "rspec"
+
+describe "searching TMDb" do
+
+  it "should call the model method that performs TMDb search" do
+
+    #To change this template use File | Settings | File Templates.
+    true.should == false
+  end
+end
